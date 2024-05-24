9e006b6082d2658daccbee228eb2612dc05efd1c
Medha Shree <medha.23bcs10049@sst.scaler.com>
2024-05-24 09:41:35 +0530

    First Changes

---
+++ src/Products.js
@@ -0,0 +1,13 @@
+import ProductCard from "./ProductCard";
+function Products(){
+    return (
+        <div>
+            I am products
+            <ProductCard />
+        </div>
+    )
+}
+
+export let a = 10;
+export let b = 20;
+export default Products;
