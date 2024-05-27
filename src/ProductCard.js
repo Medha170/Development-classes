886738dbf8a5950f6bd78bfd65af746545b80df7
Medha Shree <medha.23bcs10049@sst.scaler.com>
2024-05-27 09:26:17 +0530

    toggle

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import './ProductCard.css';
+import { useRef } from 'react';
+
 function ProductCard({title,price}){
+    let pRef = useRef(0);
     // console.log(props);
+    function productTitle(){
+        console.log(title);
+        if (pRef.current.style.display === "none"){
+            pRef.current.style.display = "block";
+        }else{
+            pRef.current.style.display = "none";
+        
+        }
+    }
     return (
-        <div>
-            <p>{title}</p>
-            <p>{price}</p>
+        <div className="product-card">
+            <p onClick={productTitle}>{title}</p>
+            <p className="price" ref={pRef}>{price}</p>
         </div>
     );
 }
